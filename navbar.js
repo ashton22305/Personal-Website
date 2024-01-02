f6fe66ad1b40f34c85555a24c6a09744474cb54f
Ashton South <aasouth223+github@gmail.com>
2024-01-02 12:58:14 -0500

    switch navbar to being generated by JS

---
+++ navbar.js
@@ -0,0 +1,10 @@
+function createNavbar() {
+    const html = '\
+        <nav id="navbar"> \
+            <a href="#">Home</a> \
+            <a href="https://github.com/ashton22305/" class="push">Github</a> \
+            <a href="#">Test</a> \
+        </nav>';
+    const navdiv = document.getElementById("navbar");
+    navdiv.innerHTML = html;
+}
