7eac1a62250f709aa99e35e6392c58305be1239a
Ashton South <aasouth223+github@gmail.com>
2024-01-02 14:26:00 -0500

    generate projects

---
+++ projects.js
@@ -0,0 +1,25 @@
+async function loadProjects() {
+    // Request projects
+    const request = new Request("./projects.json");
+    const response = await fetch(request);
+
+    const data = await response.json();
+
+    // Manipulate DOM to display projects
+    const projects = document.getElementById("projects");
+    for(const project of data) {
+        const projectbox = document.createElement("div");
+        projectbox.setAttribute("class", "project");
+        projects.appendChild(projectbox);
+
+        const title = document.createElement("h2");
+        title.textContent = project.name;
+        projectbox.appendChild(title);
+
+        const description = document.createElement("p");
+        description.textContent = project.description;
+        projectbox.appendChild(description);
+    }
+}
+
+window.addEventListener("load", loadProjects);
